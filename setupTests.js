74a327248251fce71186ff55121657919d5caa64
tomek <tomek@unknown-host.Home>
2017-05-01 15:10:48 +0100

    Added test setup, implemented giphinate handler

---
+++ setupTests.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const sinon = require('sinon')
+const chai = require('chai')
+const sinonChai = require('sinon-chai')
+
+chai.use(sinonChai)
+global.sinon = sinon
+global.expect = chai.expect
