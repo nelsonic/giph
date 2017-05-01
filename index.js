8cf0e669e9f97bc201a5b8a86d9e7e10ed25462d
tomek <tomek@unknown-host.Home>
2017-05-01 23:59:36 +0100

    eslint code fixes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // @see https://nodejs.org/api/cluster.html
-const cluster = require('cluster');
-const numCPUs = require('os').cpus().length;
+const cluster = require('cluster')
+const numCPUs = require('os').cpus().length
 const logger = require('menna')
 
 const setupExpress = require('./setupExpress')
@@ -10,23 +10,23 @@ if (cluster.isMaster) {
   const schema = require('./lib/schemas')()
   schema.Giphys.sync({ force: true })
 
-  logger.info(`Master ${process.pid} is running`);
+  logger.info(`Master ${process.pid} is running`)
 
   // Fork workers.
   for (let i = 0; i < numCPUs; i++) {
-    cluster.fork();
+    cluster.fork()
   }
 
   cluster.on('exit', (worker, code, signal) => {
-    logger.warning(`worker ${worker.process.pid} died`);
+    logger.warning(`worker ${worker.process.pid} died`)
 
     // Retry
     cluster.fork()
-  });
+  })
 } else {
   setupExpress()
 
-  logger.info(`Worker ${process.pid} started`);
+  logger.info(`Worker ${process.pid} started`)
 }
 
 // TODO: implement e2e/component tests depending on the testing strategy you want to follow
